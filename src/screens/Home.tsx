837813a9cbc4742bd264af5a75a37b569ca786c8
Nithesh <45152787+nk711@users.noreply.github.com>
2022-02-28 15:46:37 +0000

    Added Filter and stats to home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,12 +3,15 @@ import { Button, Text, TextInput } from "react-native";
 import { atom, useRecoilValue, useSetRecoilState } from "recoil";
 import { AddCard } from "../components/AddCard";
 import { Card } from "../components/Card";
+import { CardFilters } from "../components/CardFilters";
+import { CardStats } from "../components/CardStats";
 import { todoListState } from "../recoil/store";
 
 export const Home = () => {
     const todoList = useRecoilValue(todoListState);
     return (<>
-            
+                <CardStats/>
+                <CardFilters/>
                 <AddCard/>
                 { todoList.map((item)=> (
                     <Card key = {item.key} item = {item}>
